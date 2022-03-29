bfc3688c97a056809d8af0fb6a2daf8709b6345a
breno30 <brenodonascimentosilvaa@gmail.com>
2022-03-29 17:00:01 -0300

    Notificação pelo windows

--- principal.py
+++ principal.py
@@ -3,6 +3,7 @@ from time import sleep
 import speech_recognition as sr
 import pyttsx3 
 import os
+import subprocess
 
 #importa funções dos demais arquivos
 from agenda import agenda
@@ -14,6 +15,24 @@ funcoes = [agenda, desempenho, quiz]
 
 #iniciando fala
 engine = pyttsx3.init()
+
+def notificar(titulo, texto):
+    comando = """
+    [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null;
+    $Template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText02);
+    $RawXml = [xml] $Template.GetXml()
+    ($RawXml.toast.visual.binding.text|where {$_.id -eq "1"}).AppendChild($RawXml.CreateTextNode('"""+titulo+"""')) > $null
+    ($RawXml.toast.visual.binding.text|where {$_.id -eq "2"}).AppendChild($RawXml.CreateTextNode('"""+texto+"""')) > $null;
+    $SerializedXml = New-Object Windows.Data.Xml.Dom.XmlDocument;
+    $SerializedXml.LoadXml($RawXml.OuterXml);
+    $Toast = [Windows.UI.Notifications.ToastNotification]::new($SerializedXml);
+    $Toast.Tag = "PowerShell";
+    $Toast.Group = "PowerShell";
+    $Toast.ExpirationTime = [DateTimeOffset]::Now.AddMinutes(5);
+    $Notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("OTUS");
+    $Notifier.Show($Toast);
+    """
+    process=subprocess.Popen(["powershell","& {" + comando + "}"],stdout=subprocess.PIPE);
 
 def procurar_comando(frase):
     encontrado = False
@@ -65,7 +84,7 @@ def ouvir():
                     #speak('please, try again')
                     print("erro")
                     pass
-                
+                    
 while True:
     print('PRESSIONE ENTER PARA FALAR')
     record(until="enter")
