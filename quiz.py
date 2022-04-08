8470f50c338a7e259fe98c8341a2a84a2417734c
breno30 <brenodonascimentosilvaa@gmail.com>
2022-04-08 15:03:25 -0300

    Mudando função para utils

--- quiz.py
+++ quiz.py
@@ -1,48 +1,20 @@
-def perguntar(window, pergunta, respostas):
-    from tkinter import ttk
-    #Limpa tela
-    for componente in window.winfo_children():
-        componente.destroy()
-        
-    #Pergunta
-    label_pergunta = ttk.Label(window, text=pergunta,
-                                font = ('Helvetica', 15, 'bold'))
-    label_pergunta.place(relx=0.5, rely=0.1, anchor='center')
-
-
-    #Resposta
-    label_respostas = []
-    for idx, resposta in enumerate(respostas):
-        #calcula altura das respostas
-        altura_y = (idx+2)/10
-        #adiciona resposta a array
-        label_respostas.append( ttk.Label(window, text=f'{idx+1}) {resposta}',
-                                            font = ('Helvetica', 13, 'bold')) )
-        #pega última resposta (-1) adicionada
-        label_respostas[-1].place(relx=0.3, rely=altura_y, anchor='w')
-
-
-
-
-
-
 def quiz():
-
     from ttkthemes import ThemedTk
-
+    from utils import perguntar
+    
     #define tema
     window = ThemedTk(theme="arc")
 
     #define tamanho da tela
     window.geometry('400x450')
 
-    """"
+    ''' 
     #É passada a pergunta com as possíveis respostas 
     perguntar(window,
             "Qual área deseja estudar?",
             ['Web',
             'Desktop',
             'Mobile'])
-    """
-    
+    '''
+ 
     window.mainloop()
--- utils.py
+++ utils.py
@@ -1,3 +1,28 @@
+#FUNÇÕES INTERFACE ----------------------------------------------------
+def perguntar(window, pergunta, respostas):
+    from tkinter import ttk
+    #Limpa tela
+    for componente in window.winfo_children():
+        componente.destroy()
+        
+    #Pergunta
+    label_pergunta = ttk.Label(window, text=pergunta,
+                                font = ('Helvetica', 15, 'bold'))
+    label_pergunta.place(relx=0.5, rely=0.1, anchor='center')
+
+
+    #Resposta
+    label_respostas = []
+    for idx, resposta in enumerate(respostas):
+        #calcula altura das respostas
+        altura_y = (idx+2)/10
+        #adiciona resposta a array
+        label_respostas.append( ttk.Label(window, text=f'{idx+1}) {resposta}',
+                                            font = ('Helvetica', 13, 'bold')) )
+        #pega última resposta (-1) adicionada
+        label_respostas[-1].place(relx=0.3, rely=altura_y, anchor='w')
+
+
 #FUNÇÕES DE VOZ ------------------------------------------------------
 
 def tocar(audio):
