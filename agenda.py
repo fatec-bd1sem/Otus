7d87f1d2c99b46ec55014af4ce4910a7bb849487
DevBielgrazi <gabrielvieira.tcc@gmail.com>
2022-04-10 20:27:56 -0300

    Armazenando tarefa em váriaveis

--- agenda.py
+++ agenda.py
@@ -1,46 +1,46 @@
-#Ouvindo dia
-def coletar_dia():
-    from utils import ouvir
-    dia = str(ouvir())
-    return dia
-
-#Ouvindo tarefa
-def coletar_tarefa():
-    from utils import ouvir
-    tarefa = str(ouvir())
-    return tarefa
-
-#Ouvindo Tempo
-def coletar_tempo():
-    from utils import ouvir
-    tempo = str(ouvir())
-    return tempo
-
 def agenda():
-    from threading import Thread
     from ttkthemes import ThemedTk
     from utils import agendar_tarefa
+    import threading
+    
+    #Armazenando dia
+    def coletar_dia():
+        from utils import ouvir
+        dia = int(ouvir())
+        if dia != None:
+            janela2 = ThemedTk(theme="arc")
+            janela2.geometry("300x200")
+            agendar_tarefa(janela2,"Qual tarefa deseja estudar?")
+            threading.Thread(target=coletar_tarefa).start()
+            janela2.mainloop()
+        return dia
+    
+    #Armazenando tarefa
+    def coletar_tarefa():
+        from utils import ouvir
+        tarefa = str(ouvir())
+        if tarefa != None:
+            janela3 = ThemedTk(theme="arc")
+            janela3.geometry("300x200")
+            agendar_tarefa(janela3,"Quanto tempo deseja estudar?")
+            threading.Thread(target=coletar_tempo).start()
+            janela3.mainloop()
+        return tarefa
+    
+    #Armazenando tempo
+    def coletar_tempo():
+        from utils import ouvir
+        tempo = float(ouvir())
+        return tempo
 
+    
     #Tema interface
     janela = ThemedTk(theme="arc")
 
     #Tamanho interface
-    janela.geometry('250x100')
+    janela.geometry('300x200')    
 
     #Pergunta dia 
     agendar_tarefa(janela,"Qual dia deseja estudar?")
-    Thread(target=lambda:coletar_dia()).start()
+    threading.Thread(target=coletar_dia).start()
     janela.mainloop()
-    
-    #Pergunta tarefa após receber dia
-    if coletar_dia() != "":
-        janela.destroy()
-        agendar_tarefa(janela,"Qual tarefa deseja estudar?")
-        Thread(target=lambda:coletar_tarefa()).start()
-        janela.mainloop()
-        #Pergunta tempo após receber tarefa
-        if coletar_tarefa() != "":
-            janela.destroy()
-        agendar_tarefa(janela,"Quanto tempo deseja estudar?")
-        Thread(target=lambda:coletar_tempo()).start()
-        janela.mainloop()
