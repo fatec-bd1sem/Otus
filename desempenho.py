c24c92bab904c2addb9e72978c22c231c79fa221
breno30 <brenodonascimentosilvaa@gmail.com>
2022-04-13 19:37:52 -0300

    Verificando se a tarefa foi feita

--- desempenho.py
+++ desempenho.py
@@ -10,11 +10,13 @@ def desempenho():
     for dia in data:
         data[dia]
         for tarefa in data[dia]:
-            tarefa.get("horas")
-            soma_horas += tarefa.get("horas")
-            tarefa.get("tecnologia")
-            tecnologias.append(tarefa.get("tecnologia"))
-            horas.append(tarefa.get("horas"))
+            #soma hora apenas se tarefa foi feita
+            if tarefa.get("feito") == "S":
+                tarefa.get("horas")
+                soma_horas += tarefa.get("horas")
+
+                tecnologias.append(tarefa.get("tecnologia"))
+                horas.append(tarefa.get("horas"))
 
     fig1, ax1 = plt.subplots()
     #print (fig1, ax1)
@@ -25,4 +27,3 @@ def desempenho():
     ax1.set_title("horas empregadas de estudo")
 
     plt.show()
-
