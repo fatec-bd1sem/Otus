3f02f6dd8c94423b5facd0a0174cf0c055f01a15
breno30 <brenodonascimentosilvaa@gmail.com>
2022-05-21 10:39:57 -0300

    Zerando caminhos de import na calculadora

--- funcoes_calculadora/plano_cartesiano.py
+++ funcoes_calculadora/plano_cartesiano.py
@@ -1,2 +1,6 @@
 def plano_cartesiano():
+    # zerando caminho para importar funções de diretórios acima
+    import sys
+    sys.path.append('')
+    
     print('função plano_cartesiano chamada')
--- funcoes_calculadora/tabela_verdade.py
+++ funcoes_calculadora/tabela_verdade.py
@@ -1,2 +1,6 @@
 def tabela_verdade():
+    # zerando caminho para importar funções de diretórios acima
+    import sys
+    sys.path.append('')
+    
     print('função tabela_verdade chamada')
