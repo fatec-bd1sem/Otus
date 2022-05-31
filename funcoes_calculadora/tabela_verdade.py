ad7f238ce48aaee0c1e3902638ed3215eaade501
beatrice <beatricelopes@outlook.com>
2022-05-31 20:54:11 -0300

    inicio tabela verdade

--- funcoes_calculadora/tabela_verdade.py
+++ funcoes_calculadora/tabela_verdade.py
@@ -1,6 +1,45 @@
 def tabela_verdade():
     # zerando caminho para importar funções de diretórios acima
+    import ttg
     import sys
     sys.path.append('')
     
-    print('função tabela_verdade chamada')
+    from utils import ouvir, falar
+    falar('Qual tabela-verdade você deseja visualizar?')
+    frase = ouvir()
+
+    ##tabelas verdade
+    def conjuncao(): 
+        print(ttg.Truths(['p', 'q'], ['p and q']))
+
+    def disjuncao():
+        print(ttg.Truths(['p', 'q'], ['p or q']))
+    
+    def implicacao():
+        print(ttg.Truths(['p', 'q'], ['p xor q']))
+    
+    def bicondicional():
+        print(ttg.Truths(['p', 'q'], ['p = q']))
+    
+    def exclusive_disjunction():
+        print(ttg.Truths(['p', 'q'], ['p != q']))
+
+    
+
+    palavras = frase.split()
+    palavras_chave = ['e', 'ou']
+    funcoes = [conjuncao, disjuncao]
+    
+    for palavra in palavras:
+        if palavra in palavras_chave:
+            comando = palavras_chave.index(palavra)
+            funcoes[comando]()
+            break
+
+
+
+        
+
+
+
+
